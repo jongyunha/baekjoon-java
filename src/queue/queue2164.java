1595e80ecbf684f04213cc446b15fdb84a096033
nothingprogram <dev@jongyun.com>
2022-03-10 22:33:53 +0900

    Card 2

---
+++ src/queue/queue2164.java
@@ -0,0 +1,31 @@
+package queue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+public class queue2164 {
+
+    private final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private final static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while (queue.size() > 1) {
+            queue.pollFirst();
+            int first = queue.pollFirst();
+            queue.addLast(first);
+        }
+        bw.write(queue.getFirst().toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
